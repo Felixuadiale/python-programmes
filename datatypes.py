8a705d0889a083ed86690836647475e170478357
Felixuadiale <kennedoc@gmail.com>
2024-11-07 17:02:43 +0100

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+name = "penguin"
+age=15
+is_student=True
+weight=38.5
+print("name ",name)
+print("datatype of name",type(name ))
+print("name ",age)
+print("datatype of name",type(age ))
+print("name ",is_student)
+print("datatype of name",type(is_student))
+
