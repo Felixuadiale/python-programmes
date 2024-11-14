e283c9be98ee396d2070d9f35ebe1164ff683e0e
Felixuadiale <kennedoc@gmail.com>
2024-11-14 16:40:26 +0100

    Create negative positve and nueral.py

---
+++ negative positve and nueral.py
@@ -0,0 +1,8 @@
+number= int(input( "Enter number to check"))
+print("Number to be checked:,")
+if number>0:
+    print("This is a positive number")
+elif number<0:
+ print("This is a negative number")
+else:
+   print("This is neutral")
