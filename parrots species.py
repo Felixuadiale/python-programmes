45867695ec3b1099d6bfb676964d6110e29c49b0
Felixuadiale <kennedoc@gmail.com>
2024-11-22 16:44:56 +0100

    Create parrots species.py

---
+++ parrots species.py
@@ -0,0 +1,11 @@
+class parrot:
+    species="board"
+    def __init__(self,name , age):
+        self.name=name 
+        self.age=age
+blue=parrot("blue",10)
+woo=parrot("woo",15)
+print("Blue is a {} ". format(blue.species))
+print("Woo is a {} ". format(woo.species))
+print("{} is {} years old".format(blue.name,blue. age))
+print("{} is {} years old".format(woo.name,woo. age))
