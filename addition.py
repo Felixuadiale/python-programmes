cfd95344202a7090b39fc47927ed4000ac692b9d
Felixuadiale <kennedoc@gmail.com>
2024-11-23 16:02:37 +0100

    m

--- addition.py
+++ addition.py
@@ -1,6 +1,6 @@
-number= int(input( "Enter number to check"))
-print("Number to be checked:,",number)
-if number%2==0:
-    print("This is a even number")
-else:
-    print("This is an odd number")
+num1 = input('Enter first number: ')
+num2 = input('Enter the second number: ')
+
+sum = float (num1) + float(num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
---
+++ nultiplication.py
@@ -0,0 +1,4 @@
+num_1 = 89
+num_2 = 234
+product = num_1 * num_2
+print("Product of {} and {} is {}".format(num_1, num_2,product))
