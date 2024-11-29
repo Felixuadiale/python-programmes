c3639407830ed6daf5ea9c90a40331343bb94593
Felixuadiale <kennedoc@gmail.com>
2024-11-29 17:02:35 +0100

    9

---
+++ my dictionary.py
@@ -0,0 +1,11 @@
+mydict={}
+mydict={1:"apple",
+        2:"ball"}
+print("Dictionary",mydict)
+mydict={"Name": "John", 1:[2,3,4]}
+mydict= {"name": "Jack", "age":26}
+mydict["address"]="Downtown"
+print("my_dict")
+mydict.pop("age")
+print("my_dict")
+print("address:",mydict.get("address"))
---
+++ tuples.py
@@ -0,0 +1,4 @@
+mytuple=(1,"hello",3.4)
+print(mytuple)
+mytuple=("mouse",[8,4,6],(1,2,3))
+print(mytuple[0])
