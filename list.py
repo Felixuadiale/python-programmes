f1959628b1dec3f6c31ae92d3f089c86b5be694e
Felixuadiale <kennedoc@gmail.com>
2025-01-31 16:58:52 +0100

    n

---
+++ list.py
@@ -0,0 +1,8 @@
+emptylist=[]
+numbers=[1,2,4,5,7,6,8,9,10]
+print(numbers)
+triples=[1,2,4]* 3
+print(triples)
+aList =[100,200,400,500,700,600,800,900,1000]
+aList=aList=aList[::-1]
+print(aList,"\n") 
