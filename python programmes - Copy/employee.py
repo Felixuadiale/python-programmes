dfad3da271bfac87a3e78ed28b0c5fe09d6ad254
Felixuadiale <kennedoc@gmail.com>
2025-02-21 17:03:43 +0100

    Create employee.py

---
+++ python programmes - Copy/employee.py
@@ -0,0 +1,13 @@
+class Employeee:
+    def __init__ (self):
+        print("Employees created")
+        def __del__ (self):
+            print("Destructor called")
+            def Create_obj(): print("Making Object....")
+            obj=Employee() 
+            print("function end...")
+            return obj
+        
+        print ("Calling Create_obj()function...")
+        obj =Create_obj()
+        print("Program End...")
