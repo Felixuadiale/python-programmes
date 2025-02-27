b490366dfe59068d5f93bc9df8dac41005d27e8b
Felixuadiale <kennedoc@gmail.com>
2025-02-27 17:01:42 +0100

    g
    employ

--- python programmes - Copy/employee.py
+++ python programmes - Copy/employee.py
@@ -1,13 +1,15 @@
-class Employeee:
+class Employee:
     def __init__ (self):
         print("Employees created")
-        def __del__ (self):
+    def __del__ (self):
             print("Destructor called")
-            def Create_obj(): print("Making Object....")
+
+def Create_obj(): 
+            print("Making Object....")
             obj=Employee() 
             print("function end...")
             return obj
         
-        print ("Calling Create_obj()function...")
-        obj =Create_obj()
-        print("Program End...")
+print ("Calling Create_obj()function...")
+obj =Create_obj()
+print("Program End...")
