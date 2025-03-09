d6bc90ec3789f4184fa0e5912085521e60410598
Felixuadiale <kennedoc@gmail.com>
2025-03-09 15:34:28 +0100

    Update roman.py

--- python programmes - Copy/roman.py
+++ python programmes - Copy/roman.py
@@ -2,7 +2,7 @@ class solution:
     def IntToRoman(self,num: int)-> str:
         symList = [["I",1]],[["II",2]],[["III",3]],[["IV",4]],[["V",5]],[["VII",6]],[["VII",7]]
         res+""
-        for sym, val in reversed(symLSist):
+        for sym, val in reversed(symList):
             if num // val:
                 count = num // val
                 res +=(sym* count)
