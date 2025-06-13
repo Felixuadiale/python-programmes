23fb4e1f954b88fe9e4a09cf88f45a2aabe82c11
Felixuadiale <kennedoc@gmail.com>
2025-06-13 17:10:27 +0100

    ccccvvvvvvvcnjfjcc

---
+++ python programmes - Copy/2Equal numbers.py
@@ -0,0 +1,13 @@
+def checkIfSame(number1, number2):
+    if  ((number1 ^ number2) != 0):
+        
+        print("Numbers are not equal")
+
+    else:
+        print("Numbers are not equal")
+
+number1 = int(input("Enter first umber to compare :"))
+number2 = int(input("Enter second number to compar :"))
+
+checkIfSame(number1, number2)
+
