6daa389ff1badeafeeb7b60cf9aec063441af97f
Felixuadiale <kennedoc@gmail.com>
2025-08-22 17:00:40 +0100

    j

--- python programmes - Copy/lineChart.py
+++ python programmes - Copy/lineChart.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 x=[0,5,10,15,20,25,30]
 y1=[10,15,20,25,30,15,0]
-y2=[10,12.16,12,2,10,0]
+y2=[10,12,16,12,2,10,0]
 plt.plot(x,y1,linestyle="dashed",marker='D')
 plt.plot(x,y2,linestyle="dashed",marker='D')
 plt.title('velocity-Time Graph')
