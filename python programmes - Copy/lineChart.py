dc869ce39bfa8fcd971631941aa7ad96769466a9
Felixuadiale <kennedoc@gmail.com>
2025-08-25 11:43:27 +0100

    Update lineChart.py

--- python programmes - Copy/lineChart.py
+++ python programmes - Copy/lineChart.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
-x=[0,5,10,15,20,25,30]
+x=[0,10,15,20,25,30,35, 40]
 y1=[10,15,20,25,30,15,0]
 y2=[10,12,16,12,2,10,0]
 plt.plot(x,y1,linestyle="dashed",marker='D')
 plt.plot(x,y2,linestyle="dashed",marker='D')
 plt.title('velocity-Time Graph')
-plt.xlabel('velocity m/s')
-plt.ylabel('Time(s)')
-plt.xlim(5,25)
-plt.ylim(5,25)
+plt.xlabel('Time(s)')
+plt.ylabel('Velocity m/s')
+plt.xlim(10,35)
+plt.ylim(10,35)
 
 plt.show()
